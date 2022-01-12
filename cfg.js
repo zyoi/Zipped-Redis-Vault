82fa30ee3936b8547f779b54ed8157ec5cd3a119
shin <remerte@gmail.com>
2022-01-12 23:01:07 +0200

    lets fucking go

---
+++ cfg.js
@@ -0,0 +1,1 @@
+const list = [  'cookies', 'password','steam_cookies', 'steam_api_key',  'bits_api_key', 'bits_secret', 'steam_id', 'login']module.exports = {  secret_keys: {    csm_accounts: list,    csm_accounts_no_inv: list,    csm_accounts_full_info: list  }}
---
+++ dasda.js
@@ -0,0 +1,1 @@
+const SHA256 = require("crypto-js/sha256");let data = [  { foo: "foo", bar: "bar", baz: "baz" },  { foo: "foo", bar: "bar", baz: "baz" },  { foo: "foo", bar: "bar", baz: "baz" },  { foo: "foo", bar: "bar", baz: "baz" }];let foo = JSON.stringify(data);console.log(SHA256(foo).toString());
