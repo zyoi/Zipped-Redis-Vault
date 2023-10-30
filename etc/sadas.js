b0707f0dca83aa6fd053ad2d05cafd5fdc1bd2f7
shin <remerte@gmail.com>
2023-10-30 18:08:12 +0400

    old files

--- etc/sadas.js
+++
@@ -1,6 +0,0 @@
-const db = require('../index')
-
-//db.watcher('accounts', true, console.log)
-db.set('accounts', [{'as': 's'}]).then(() => {
-  db.get('accounts').then(console.log).catch(console.error)
-}).catch(console.error)
--- test.js
+++
@@ -1,9 +0,0 @@
-const db = require('./index');
-const log = console.log.bind(console);
-
-(async () => {
-  log(await db.get('testwe'))
-  await db.set('testwe', [{test: 'awoo'}])
-
-  log(await db.get('testwe'))
-})()
